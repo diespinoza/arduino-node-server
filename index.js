7ace8acd1a97e83948ef046532d1f9c09197eaf2
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-09-29 23:22:20 -0400

    Linked the header and footer to client

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var path = require("path");
 //var MongoClient = require("mongodb").MongoClient
 var app = express();
 
-//Tells express that EJS will be the templating engine
-app.set("view engine", "ejs");
 //Tells express that views will be in the folder called views
 app.set("views", path.resolve(__dirname, "views"));
+//Tells express that EJS will be the templating engine
+app.set("view engine", "ejs");
 //Tells express to use morgan for logging
 app.use(logger("short")); //"short" returns a function
 //Tells express to use json
