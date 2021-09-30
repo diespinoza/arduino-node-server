57dba20274225628c7d1cd6768361cf89508203a
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-09-29 22:34:41 -0400

    deleted some files. modified main index file, added styling

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 var express = require("express");
 var http = require("http");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 var path = require("path");
-var MongoClient = require("mongodb").MongoClient
-
+//var MongoClient = require("mongodb").MongoClient
 var app = express();
 
-//Tells express that views will be in the folder called views
-app.set("views", path.resolve(__dirname, "views"));
 //Tells express that EJS will be the templating engine
 app.set("view engine", "ejs");
-
-//var publicPath = path.resolve(__dirname, "public");
-//app.use(express.static(publicPath));
-
+//Tells express that views will be in the folder called views
+app.set("views", path.resolve(__dirname, "views"));
+//Tells express to use morgan for logging
 app.use(logger("short")); //"short" returns a function
+//Tells express to use json
+app.use(bodyParser.json())
+//Tells express to serve make the public folder accessible
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+//animations variable
+var animations = [];
+app.locals.animations = animations;
 
 app.get("/", function(request, response){
-  response.end("Hello, this is the homepage!");
+  response.render("client")
+  //response.end("Hello, this is the homepage!");
 });
 
 app.get("/blynk", function(request, response){
-  response.render("client", {
-    message: "This is where you can control the blynk program interface!! Change the color and lighting duration. Select RGB values :D"
-  });
+  response.render("blynk");
 
 });
 
 app.post("/blynk/api", function(request, response){
-  response.render("client", {
-    message: "This is where the api request will come in"
-  });
+  //recieve the request and do something with it
 
 });
 
@@ -39,20 +42,13 @@ app.get("/neopixel", function(request, response){
   response.end("This is where you can add another strip of LEDs to controll");
 });
 
+//404 page
 app.use(function(request, response){
   response.statusCode = 404;
   response.end(404);
 });
 
-/*
-app.use(function(request, response, next){
-  console.log("In comes a request to: " + request.url);
-  next();
+//Start server
+http.createServer(app).listen(3000, () => {
+  console.log('Started server on port 3000')
 });
-app.use(function(request, response){
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Worlds");
-});
-*/
-
-http.createServer(app).listen(3000);
