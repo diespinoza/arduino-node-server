a9f894b6531502c8a8e343230ae4de576e9df05c
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-01 20:31:52 -0400

    added form and POST

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ var path = require("path");
 //var MongoClient = require("mongodb").MongoClient
 var app = express();
 
-//Tells express that views will be in the folder called views
-app.set("views", path.resolve(__dirname, "views"));
-//Tells express that EJS will be the templating engine
-app.set("view engine", "ejs");
-//Tells express to use morgan for logging
-app.use(logger("short")); //"short" returns a function
-//Tells express to use json
-app.use(bodyParser.json())
-//Tells express to serve make the public folder accessible
+app.set("views", path.resolve(__dirname, "views")); //views are in folder ./views
+app.set("view engine", "ejs"); //ejs is the templating engine
+app.use(logger("short")); //Use Morgan for logging
+app.use(bodyParser.json()) //Use JSON
+app.use(bodyParser.urlencoded({extended: true})) //Places form data into body of request
 var publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); //Make the public folder accessible
 
 //animations variable
 var animations = [];
@@ -27,13 +23,15 @@ app.get("/", function(request, response){
   //response.end("Hello, this is the homepage!");
 });
 
+//remove this blynk page
 app.get("/blynk", function(request, response){
   response.render("blynk");
 
 });
 
-app.post("/blynk/api", function(request, response){
+app.post("/addAnimation", function(request, response){
   //recieve the request and do something with it
+  console.log(request.body)
 
 });
 
