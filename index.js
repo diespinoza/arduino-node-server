77c3b76e763c0f2e31a4d20a5403d88faaa0a082
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-10 21:08:21 -0400

    added size limit to db. changed ejs

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 const client = new MongoClient(dbConnectionStr, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
-      // perform actions on the collection object
-  //   client.close();
-  //
+// perform actions on the collection object
+//   client.close();
+//
 });
 */
 
@@ -40,16 +40,17 @@ app.use(bodyParser.urlencoded({extended: true})) //Places form data into request
 var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath)); //Make the public folder accessible
 
-//animations variable
-var animations = [];
-app.locals.animations = animations;
+//animations count in database variable
+var animationsCount = 0;
+//app.locals.animations = animations;
 
 app.get("/", function(request, response){
   db.collection('device1').find().toArray()
     .then(data => {
       console.log('retrieved data from database')
-      console.log(data)
-      response.render("client")
+      animationsCount = data.length
+      console.log(animationsCount)
+      response.render("client", {info: data})
     })
     .catch(error => console.error(error))
   //console.log('rendered' + request.originalUrl)
@@ -63,14 +64,20 @@ app.get("/blynk", function(request, response){
 
 app.post("/addAnimation", function(request, response){
   //recieve the request and add inserOne to database
-  console.log(request.body)
-  db.collection('device1').insertOne({name: request.body.animation,
-  delay: request.body.delay})
-    .then(result => {
-      console.log("Animation added to db")
-      response.redirect('/')
-    })
-    .catch(error => console.error(error))
+  console.log("Check animation count:", animationsCount)
+ // console.log(animationsCount)
+  if(animationsCount <= 5){
+    db.collection('device1').insertOne({name: request.body.animation,
+      delay: request.body.delay})
+      .then(result => {
+        console.log("Animation added to db")
+        response.redirect('/')
+      })
+      .catch(error => console.error(error))
+  } else {
+    console.log("too many animations")
+    response.redirect('/')
+  }
 
 });
 
