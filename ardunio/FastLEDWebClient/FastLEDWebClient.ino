6e9ef886d3b8abb4ecd1ca399ff6dbc7c4ded46b
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-16 00:32:46 -0400

    added led strip functions. both wifinina and fastled working

--- ardunio/FastLEDWebClient/FastLEDWebClient.ino
+++ ardunio/FastLEDWebClient/FastLEDWebClient.ino
@@ -1,14 +1,16 @@
 /*
-  Web client
+  Fast LED Web client
 
  This sketch connects to the arduino neopixel api
- using the WiFi module.
+ using the WiFi module. It uses the FastLED library to control a string
+ of RGB LEDs
 
  This sketch is written for a network using WPA encryption. For
  WEP or WPA, change the WiFi.begin() call accordingly.
 
  Circuit:
  * Board with NINA module (Arduino UNO WiFi Rev.2)
+ * WS2812 LED strip (any length)
 
 Based off WifiWebClient example
 
@@ -19,8 +21,10 @@ Based off WifiWebClient example
 
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <FastLED.h>
 
 #include "arduino_secrets.h" 
+/****************** WiFI Client Settings  ******************/
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
@@ -36,6 +40,24 @@ int port = 80;
 // Initialize the WiFi client library
 WiFiClient client;
 
+/****************** LED Strip Settings  ******************/
+// How many leds in your strip? How bright (Max: 255)?
+#define NUM_LEDS 50 
+#define BRIGHTNESS 95
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 6
+//#define CLOCK_PIN 13
+#define LED_TYPE WS2812
+#define COLOR_ORDER RGB
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+
+/****************** Program Start  ******************/
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
@@ -55,6 +77,13 @@ void setup() {
     Serial.println("Please upgrade the firmware");
   }
 
+  Serial.println("Starting LEDs");
+  //Initialize the LED strip. Use ypical8mmPixel or TypicalPixelString for setCorrection
+  LEDS.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(Typical8mmPixel); 
+  LEDS.setBrightness(BRIGHTNESS);
+
+  fill_solid(leds, 50, CRGB::Red);
+  FastLED.show();
   // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
@@ -81,6 +110,14 @@ void setup() {
   }
 }
 
+/***** FastLED List of Animations *****/
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, cylon};
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+/***** The main LOOP *****/
 void loop() {
   // if there are incoming bytes available
   // from the server, read them and print them:
@@ -95,8 +132,21 @@ void loop() {
     Serial.println("disconnecting from server.");
     client.stop();
 
-    // do nothing forevermore:
-    while (true);
+    // Control the LEDs forever
+    while (true){
+      // Call the current pattern function once, updating the 'leds' array
+      gPatterns[gCurrentPatternNumber]();
+    
+      // send the 'leds' array out to the actual LED strip
+      FastLED.show();  
+      // insert a delay to keep the framerate modest
+      FastLED.delay(1000/120); 
+    
+      // do some periodic updates
+      EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+      EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+
+    }
   }
 }
 
@@ -116,4 +166,57 @@ void printWifiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+/***** FastLED Animation Functions *****/
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern(){
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void fadeall(){
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i].nscale8(250); 
+  } 
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+  FastLED.show();
+}
+
+void cylon(){
+  static uint8_t hue = 0;
+  Serial.print("x");
+  // First slide the led in one direction
+  for(int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show(); 
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
+  Serial.print("x");
+
+  // Now go in the other direction.  
+  for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }   
+
 }
