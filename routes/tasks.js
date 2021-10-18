232bf3b5ec2f6e6db4f1e113b29b7397b4b0a70a
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-18 00:53:33 -0400

    planning new api layout

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,6 +6,27 @@ const {
   getAllTasks,
 } = require('../controllers/tasks');
 
+/* What will this API look like?
+ * For a web client:
+ *  app.get('/')
+ *  serve a home page. has actions for CRUD
+ *
+ * app.get('/api/v1/animations')
+ *  get all the animations
+ *
+ *  app.post('/api/v1/animations')
+ *  create a new animation
+ *
+ *  app.get('/api/v1/animations/:id')
+ *  get a single animation
+ *
+ *  app.post('/api/v1/animations')
+ *  create a new animation
+ *
+ * For an arduino client device:
+ *
+ *
+ */
 
 router.route('/').get(getAllTasks);
 
