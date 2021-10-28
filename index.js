f229e878d115086f67c2f0b9e67ef0ed3c89eafb
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-10-28 01:34:18 -0400

    removed mongodb code, 404 page

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
+const app = express();
 // const http = require("http");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const logger = require("morgan");
 const path = require("path");
 const connectDB = require('./db/connect');
-const app = express();
 //const { MongoClient  } = require('mongodb');
 // const MongoClient = require("mongodb").MongoClient
-
-//tasks route
-const tasks = require('./routes/tasks');
-app.use('/api/v1/animations', tasks);
 
 
 const PORT = 3000;
@@ -23,34 +19,21 @@ let dbConnectionStr = process.env.DB_STRING;
 let dbName = 'fastled-animations'
 
 
-// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-//   .then(client => {
-//     console.log(`Connected to ${dbName} Database`)
-//     db = client.db(dbName)
-//   })
-
-/*
-const client = new MongoClient(dbConnectionStr, { useNewUrlParser: true, useUnifiedTopology: true  });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-// perform actions on the collection object
-//   client.close();
-//
-});
-*/
-
-
 app.set("views", path.resolve(__dirname, "views")); //views are in folder ./views
 app.set("view engine", "ejs"); //ejs is the templating engine
 app.use(logger("short")); //Use Morgan for logging
-app.use(bodyParser.json()) //Use JSON
-app.use(bodyParser.urlencoded({extended: true})) //Places form data into request body
+
+//removing body parser use express intead
+app.use(express.json());
+// app.use(bodyParser.json()) //Use JSON
+// app.use(bodyParser.urlencoded({extended: true}))
+
+//Make the public folder accessible
 var publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath)); //Make the public folder accessible
+app.use(express.static(publicPath));
 
 //animations count in database variable
 var animationsCount = 0;
-//app.locals.animations = animations;
 
 app.get("/", function(request, response){
   db.collection('device1').find().toArray()
@@ -67,7 +50,7 @@ app.get("/", function(request, response){
 
 app.post("/addAnimation", function(request, response){
   // console.log("Check animation count:", animationsCount)
- // Check if there are too many animations
+  // Check if there are too many animations
   if(animationsCount < 30){
     db.collection('device1').insertOne({name: request.body.animation,
       delay: request.body.delay})
@@ -87,7 +70,7 @@ app.delete('/deleteAnimation', (request, response) => {
   console.log(request.body.animationD);
   console.log(request.body.delayD);
   db.collection('device1').deleteOne({name: request.body.animationD,
-  delay: request.body.delayD})
+    delay: request.body.delayD})
     .then(result => {
       console.log('Animation Deleted')
       response.json('Animation Deleted')
@@ -107,21 +90,21 @@ app.get("/api/devices/:deviceId", function(request, response){
         response.json({functions: "none", delay: ""})
       } else {
         //This function is too slow. O(n) plus creating strings
-  /*
-        //join all the function names and delays into comma separated string
-        //This simplifies parsing for the arduino. not sure if fast
-        //or really necessary.
+        /*
+          //join all the function names and delays into comma separated string
+          //This simplifies parsing for the arduino. not sure if fast
+          //or really necessary.
         let names = delays = "";
         for(let i = 0; i < data.length; i++){
           names += data[i].name + ",";
           delays += data[i].delay + ",";
         }
-       // const keys = Object.keys(data[0])
+        // const keys = Object.keys(data[0])
         console.log(names)
         console.log(delays)
         //Respond with the animations.
         response.json({functions: names, delay: delays});
-  */
+        */
         response.json(data);
       }
     })
@@ -129,13 +112,22 @@ app.get("/api/devices/:deviceId", function(request, response){
 
 });
 
-//404 page
-app.use(function(request, response){
-  response.statusCode = 404;
-  response.end(404);
-});
+//this page is broken
+// 404 page
+// app.use(function(request, response){
+//   response.statusCode = 404;
+//   response.end(404);
+// });
 
 
+
+//tasks route
+const tasks = require('./routes/tasks');
+app.use('/api/v1/animations', tasks);
+
+
+//function that will start the server after
+//  connecting to the DB
 const start = async () => {
   try{
     await connectDB(dbConnectionStr);
