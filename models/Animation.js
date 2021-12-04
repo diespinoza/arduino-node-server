ef3112b1f93b5405aa6364956cddc591a9f47438
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-12-03 23:52:42 -0500

    Changed largest number to hex. Still having an issue with validation

--- models/Animation.js
+++ models/Animation.js
@@ -13,7 +13,12 @@ const AnimationSchema = new mongoose.Schema({
     required: [true, 'must provide delay'],
     default: 10,
     min: 0,
-    max: 9223372036854775807 //not working properly
+    max: 0xFFFFFFFFFFFFFFFF
+    //Largest number arduino accepts it long long or 64 bytes
+    //Represented by 16 letter F's in hex.
+    //In decimcal it should be :18446744073709551615.
+    //Website rounds up to:  18446744073709552000
+    //not sure why, maybe error in storing the variable somewhere or in rendering the ejs
   },
 });
 
